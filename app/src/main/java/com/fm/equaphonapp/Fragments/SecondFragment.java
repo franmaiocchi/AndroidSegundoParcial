53965892d8701ce812e2ee16a0664e7789bec38c
F.Maiocchi <franmaiocchi@est.frba.utn.edu.ar>
2018-11-05 17:12:20 -0300

    Cargue los productos de powersoft touring

--- app/src/main/java/com/fm/equaphonapp/Fragments/SecondFragment.java
+++ app/src/main/java/com/fm/equaphonapp/Fragments/SecondFragment.java
@@ -1,9 +1,11 @@
 package com.fm.equaphonapp.Fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -53,7 +55,6 @@ public class SecondFragment extends Fragment
     public void onEvent(MessageEvent event)
     {
         this.brand = event.message;
-        EventBus.getDefault().removeStickyEvent(event);
     }
 
     @Override
@@ -66,21 +67,59 @@ public class SecondFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
+        // Antes que nada veo que marca se clickeo!
+        MessageEvent stickyEvent = EventBus.getDefault().getStickyEvent(MessageEvent.class);
+
+        if(stickyEvent != null) {
+            // "Consume" the sticky event
+            EventBus.getDefault().removeStickyEvent(stickyEvent);
+            this.brand = stickyEvent.message;
+        }
+
+/*        // create ContextThemeWrapper from the original Activity Context with the custom theme
+        final Context contextThemeWrapper = new ContextThemeWrapper(getActivity(), R.style.Theme_STS);
+
+        // clone the inflater using the ContextThemeWrapper
+        LayoutInflater localInflater = inflater.cloneInContext(contextThemeWrapper);
+
+        // inflate the layout using the cloned inflater, not default inflater
+        View view = localInflater.inflate(R.layout.fragment_second, container, false);*/
+
+        getActivity().setTheme(R.style.Theme_STS);
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_second, container, false);
 
-        data = new ArrayList<STS>();
-        data.add(new STS("Cantata", R.drawable.cantata));
-        data.add(new STS("Coax12", R.drawable.coax12));
-        data.add(new STS("Coax15", R.drawable.coax15));
-        data.add(new STS("Concerto SUB", R.drawable.concerto));
-        data.add(new STS("infraSUB", R.drawable.infra));
-        data.add(new STS("miniSUB", R.drawable.mini));
-        data.add(new STS("Sonata", R.drawable.sonata));
-        data.add(new STS("Concerto TOP", R.drawable.top));
-        data.add(new STS("Linea V10", R.drawable.v10));
-        data.add(new STS("Linea V10i", R.drawable.v10i));
-        data.add(new STS("Linea V5", R.drawable.v5));
+        if (this.brand.equals("Powersoft"))
+        {
+            data = new ArrayList<STS>();
+            data.add(new STS("K2", R.drawable.k2));
+            data.add(new STS("K3", R.drawable.k3));
+            data.add(new STS("K6", R.drawable.k6));
+            data.add(new STS("K10", R.drawable.k10));
+            data.add(new STS("K20", R.drawable.k20));
+            data.add(new STS("M14D", R.drawable.m14d));
+            data.add(new STS("M20D", R.drawable.m20d));
+            data.add(new STS("M30D", R.drawable.m30d));
+            data.add(new STS("M28Q", R.drawable.m28q));
+            data.add(new STS("M50Q", R.drawable.m50q));
+            data.add(new STS("X4", R.drawable.x4));
+            data.add(new STS("X8", R.drawable.x8));
+        }
+        else
+        {
+            data = new ArrayList<STS>();
+            data.add(new STS("Cantata", R.drawable.cantata));
+            data.add(new STS("Coax12", R.drawable.coax12));
+            data.add(new STS("Coax15", R.drawable.coax15));
+            data.add(new STS("Concerto SUB", R.drawable.concerto));
+            data.add(new STS("infraSUB", R.drawable.infra));
+            data.add(new STS("miniSUB", R.drawable.mini));
+            data.add(new STS("Sonata", R.drawable.sonata));
+            data.add(new STS("Concerto TOP", R.drawable.top));
+            data.add(new STS("Linea V10", R.drawable.v10));
+            data.add(new STS("Linea V10i", R.drawable.v10i));
+            data.add(new STS("Linea V5", R.drawable.v5));
+        }
 
         // Set up the toolbar
         setUpToolbar(view);
@@ -105,6 +144,7 @@ public class SecondFragment extends Fragment
         if (activity != null)
         {
             activity.setSupportActionBar(toolbar);
+            activity.getSupportActionBar().setTitle(this.brand);
         }
     }
 
@@ -119,4 +159,5 @@ public class SecondFragment extends Fragment
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
     }
+
 }
